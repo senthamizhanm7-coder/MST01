8668a575835b06659ae9bee44aa23f8129f45757
Senthamizhan M <senthamizhan.m@athulyaseniorcare.com>
2026-07-17 16:28:16 +0530

    Initial Laravel Jenkins pipeline setup

---
+++ resources/views/jenkins-test.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jenkins Test</title>
+</head>
+<body>
+
+    <h1>Laravel Jenkins Pipeline Working</h1>
+
+    <p>
+        Build Status: SUCCESS
+    </p>
+
+    <p>
+        Application: Jenkins Demo Laravel Project
+    </p>
+
+</body>
+</html>
